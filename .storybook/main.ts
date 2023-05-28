bb3b29bd0dd50519b5aff7fffa1f28a10c630ae2
jura4mal <65135501+jura4mal@users.noreply.github.com>
2023-05-28 10:00:23 +0300

    sb

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,27 +1,24 @@
-import { StorybookConfig } from '@storybook/core-common'
-import * as path from 'path'
-
-
+import { StorybookConfig } from '@storybook/core-common';
+import * as path from 'path';
 const config: StorybookConfig = {
   staticDirs: ['./public'],
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        postCss: true
-      }
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-a11y', {
+    name: '@storybook/addon-styling',
+    options: {
+      postCss: true
     }
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5'
+  }, '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
   },
-  webpackFinal: (config: { resolve: { alias: any } }) => {
+  core: {},
+  webpackFinal: (config: {
+    resolve: {
+      alias: any;
+    };
+  }) => {
     if (config.resolve) {
       config.resolve.alias = {
         ...config.resolve.alias,
@@ -29,9 +26,10 @@ const config: StorybookConfig = {
         '@/ui/icons': path.resolve(__dirname, '../src/components/icons')
       };
     }
-
     return config;
+  },
+  docs: {
+    autodocs: true
   }
 };
-
 export default config;
